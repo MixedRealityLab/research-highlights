fd6b537a3c106441732a28a57fc52c4b62a15285
Martin Porcheron <martin@porcheron.io>
2014-11-11 13:08:53 +0000

    Minor tweaks

--- src/highlights/config.php
+++ src/highlights/config.php
@@ -1,14 +1,16 @@
 <?php
 
+error_reporting (E_ALL); ini_set ('display_errors', '1');
+
 define ('DIR_AJX', DIR . '/app/ajax');
 define ('DIR_PAG', DIR . '/app/page');
 define ('DIR_TPL', DIR . '/app/tpl');
-define ('DIR_DAT', DIR . '/dat');
+define ('DIR_DAT', dirname (DIR) . '/dat');
 define ('DIR_LIB', DIR .  '/sys/lib');
 define ('DIR_USR', DIR . '/usr');
 
 define ('DOMAIN', 'https://www.porcheron.uk');
-define ('PATH', '/cdt/rehi-view');
+define ('PATH', '/cdt/rh');
 
 define ('URI_HOME', DOMAIN . PATH);
-define ('URI_DATA', URI_HOME . '/dat');
+define ('URI_DATA', dirname (URI_HOME) . '/dat');
