53376c5d97d4f70ac384b122d799492c68a5e5f2
Martin Porcheron <martin@porcheron.io>
2014-11-11 11:47:33 +0000

    More common elements

--- src/submission/app/js/index.js
+++ src/submission/app/js/index.js
@@ -1,18 +1,7 @@
-$.fn.reverse = [].reverse;
-
 $(function() {
-	$('.collapse').each(function(i) {
-		$(this).delay(200 * i).fadeIn();
-	});
+	ReHi.fadePageIn();
 
 	$('#submit').click(function() {
-		var c = $('.collapse').length - 1; 
-		$('.collapse').reverse().each(function(i, v) {
-			$(v).delay(200 * i).fadeOut();
-			if (i == c) {
-				setTimeout(function() { window.location = 'submit'; }, 400 * (i+1));
-			}
-		});
+		ReHi.fadePageOut(function() { window.location = 'submit'; });
 	});
-
 });
