71c1dd0e00158684897c5d1db70aa6c11bdf896a
Martin Porcheron <martin@porcheron.io>
2015-01-13 15:18:20 +0000

    Tweets output in CSV format

---
+++ src/web/page/tweets.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Research Highlights engine
+ * 
+ * Copyright (c) 2014 Martin Porcheron <martin@porcheron.uk>
+ * See LICENCE for legal information.
+ */
+
+// Fetch all tweets
+$rh = \CDT\RH::i();
+$oSubmissionModel = $rh->cdt_submission_model;
+$oUserModel = $rh->cdt_user_model;
+
+\header ('Content-Type: text/csv');
+
+$oUsers = $oUserModel->getAll ();
+foreach ($oUsers as $oUser) {
+	$temp = $oSubmissionModel->get ($oUser->username, false);
+
+	if (isSet ($temp->text)) {
+		print $oUser->name . ',' . $oUser->email . ',' . $temp->tweet . "\n";
+	}
+}
