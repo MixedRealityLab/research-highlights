addc9bec6dfcbd469958208f8ca7afa0bfc5942d
Martin Porcheron <martin@porcheron.io>
2015-01-13 13:32:25 +0000

    Disable zooming on mobile devices

--- src/web/tpl/2015.php
+++ src/web/tpl/2015.php
@@ -16,7 +16,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<title>Research Highlights | Horizon CDT</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0">
 		<base href="<?php print URI_HOME; ?>/">
 		<link rel="stylesheet" href="<?php print URI_SYS; ?>/css/bootstrap.min.css">
 		<link rel="stylesheet" href="<?php print URI_WEB; ?>/css/main.min.css">
