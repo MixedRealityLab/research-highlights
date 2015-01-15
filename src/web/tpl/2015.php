0651be370d0a9e0c2a31df44aef6792e8357087d
Martin Porcheron <martin@porcheron.io>
2015-01-15 00:12:53 +0000

    Bookmarking icons

--- src/web/tpl/2015.php
+++ src/web/tpl/2015.php
@@ -26,6 +26,22 @@
 			<?php endforeach; ?>
 		<?php endif; ?>
 		<script src="<?php print URI_SYS; ?>/js/modernizr-2.6.2-respond-1.1.0<?php print EXT_JS; ?>"></script>
+		<link rel="apple-touch-icon" sizes="57x57" href="<?php print URI_WEB; ?>/img/apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="<?php print URI_WEB; ?>/img/apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="<?php print URI_WEB; ?>/img/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="<?php print URI_WEB; ?>/img/apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="<?php print URI_WEB; ?>/img/apple-touch-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="<?php print URI_WEB; ?>/img/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="<?php print URI_WEB; ?>/img/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="<?php print URI_WEB; ?>/img/apple-touch-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="<?php print URI_WEB; ?>/img/apple-touch-icon-180x180.png">
+		<link rel="icon" type="image/png" href="<?php print URI_WEB; ?>/img/favicon-192x192.png" sizes="192x192">
+		<link rel="icon" type="image/png" href="<?php print URI_WEB; ?>/img/favicon-160x160.png" sizes="160x160">
+		<link rel="icon" type="image/png" href="<?php print URI_WEB; ?>/img/favicon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="<?php print URI_WEB; ?>/img/favicon-16x16.png" sizes="16x16">
+		<link rel="icon" type="image/png" href="<?php print URI_WEB; ?>/img/favicon-32x32.png" sizes="32x32">
+		<meta name="msapplication-TileColor" content="#2660a9">
+		<meta name="msapplication-TileImage" content="<?php print URI_WEB; ?>/img/mstile-144x144.png">
 	</head>
 	<body>
 		<!--[if lt IE 7]>
