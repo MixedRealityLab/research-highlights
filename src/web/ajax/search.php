d88166dd3765eece115c58a337ae9b61ab9102ac
Martin Porcheron <martin@porcheron.io>
2015-01-19 20:57:45 +0000

    Optimise the search preparation

--- src/web/ajax/search.php
+++ src/web/ajax/search.php
@@ -20,49 +20,51 @@ if (\is_null ($oInputModel->get ('q'))) {
 	exit;
 }
 
+// Weight factors
+$W							= array ();
+$W['author']				= 40;
+$W['keyword']				= 20;
+$W['title']					= 10;
+$W['text']['h1']			= 9;
+$W['text']['h2']			= 8;
+$W['text']['h3']			= 7;
+$W['text']['h4']			= 6;
+$W['text']['strong']		= 5;
+$W['text']['em']			= 3;
+$W['text']['blockquote']	= 3;
+
+// Usage factors; how much the weight changes by the more its used
+$U							= array ();
+$U['author']				= 1;
+$U['keyword'] 				= .95;
+$U['title'] 				= .85;
+$U['text']['h1'] 			= .90;
+$U['text']['h2'] 			= .91;
+$U['text']['h3'] 			= .92;
+$U['text']['h4'] 			= .93;
+$U['text']['strong'] 		= .94;
+$U['text']['em'] 			= .94;
+$U['text']['blockquote'] 	= .95;
+
+// Standard factors
+$F['exact_match'] 			= 1.2;
+
 // is there a cached db?
 $file = DIR_DAT . '/search-keywords.txt';
 if (!\is_file ($file) || \filemtime ($file) + KEY_CACHE > \date ('U')) {
-	// Weights
-	$weights = array();
-	$weights['author']				= 40;
-	$weights['keyword'] 			= 20;
-	$weights['title']				= 10;
-	$weights['text_h1']				= 9; // text H1
-	$weights['text_h2']				= 8; // text H2
-	$weights['text_h3']				= 7; // text H3
-	$weights['text_h4']				= 6; // text H4
-	$weights['text_strong']			= 5; // text bold
-	$weights['text_em']				= 3; // text italics
-	$weights['text_blockquote']		= 3; // text quotes
-	$weights['text']				= 2; // text
-
-	// Usage factors; how much the weight changes by the more its used
-	$useFactors = array();
-	$useFactors['author']			= 1;
-	$useFactors['keyword'] 			= .95;
-	$useFactors['title'] 			= .85;
-	$useFactors['text_h1'] 			= .90;
-	$useFactors['text_h2'] 			= .91;
-	$useFactors['text_h3'] 			= .92;
-	$useFactors['text_h4'] 			= .93;
-	$useFactors['text_strong'] 		= .94;
-	$useFactors['text_em'] 			= .94;
-	$useFactors['text_blockquote'] 	= .95;
-	$useFactors['text'] 			= .96;
-
+	
 	// Weighted factors and all keywords
 	$globalWeights = array();
 	$searchKeywords = array();
 
-	// get weight
+	// utility functions
 	function addKeywords ($keywords, $weight, $factor, $user) {
 		$keywordsA = \explode ("\n", \trim ($keywords));
-			foreach ($keywordsA as $keywordB) {
-				$keywordC = \explode (' ', \trim ($keywordB));
-				foreach ($keywordC as $keyword) {
-					addKeyword($keyword, $weight, $factor, $user);
-				}
+		foreach ($keywordsA as $keywordB) {
+			$keywordC = \explode (' ', \trim ($keywordB));
+			foreach ($keywordC as $keyword) {
+				addKeyword ($keyword, $weight, $factor, $user);
+			}
 		}
 	}
 
@@ -83,9 +85,9 @@ if (!\is_file ($file) || \filemtime ($file) + KEY_CACHE > \date ('U')) {
 		}
 	}
 
-	function getTags($tag, $text) {
+	function getTags ($tag, $text) {
 		$matches = array();
-		preg_match_all ("/<$tag>(.*?)<\/$tag>/", $text, $matches, PREG_PATTERN_ORDER);
+		\preg_match_all ("/<$tag>(.*?)<\/$tag>/", $text, $matches, PREG_PATTERN_ORDER);
     	return $matches[1];
 	}
 
@@ -94,7 +96,7 @@ if (!\is_file ($file) || \filemtime ($file) + KEY_CACHE > \date ('U')) {
 		return $user->countSubmission;
 	});
 
-
+	// catalogue the keywords
 	foreach ($oUsers as $oUser) {
 		$data = $oSubmissionModel->get ($oUser->username, false);
 
@@ -130,42 +132,58 @@ if (!\is_file ($file) || \filemtime ($file) + KEY_CACHE > \date ('U')) {
 
 // keyword database
 $db = \unserialize (\file_get_contents ($file));
-$kDb = \array_keys ($db);
+$dbK = \array_keys ($db);
 
-// load results                      
-$results = array();
+// search database
 $query = $oInputModel->get ('q');
-$qWords = \explode (' ', $query);
+$qWords = \preg_split ('/ /', $query, -1, PREG_SPLIT_NO_EMPTY);
+$results = array();
 foreach ($qWords as $qWord) {
-	$qWordL = \trim (\strtolower ($qWord));
-	if (!empty ($qWordL)) {
-		foreach ($db as $key => $row) {
-			if (\strpos ($key, $qWord) !== false) {
-				$row['qWord'] = $qWord;
-				$row['key'] = $key;
-				$results[] = $row;
-			}
+	$ms = \preg_grep ('/' . $qWord .'/', $dbK);
+	foreach ($ms as $m => $d) {
+		$k = $dbK[$m];
+		$results[$k] = $db[$k];
+		if ($k === $qWord) {
+			$results[$k]['weight'] *= $F['exact_match'];
 		}
 	}
 }
+
+
+// // load results                      
+// $results = array();
+// $query = $oInputModel->get ('q');
+// $qWords = \explode (' ', $query);
+// foreach ($qWords as $qWord) {
+// 	$qWordL = \trim (\strtolower ($qWord));
+// 	if (!empty ($qWordL)) {
+// 		foreach ($db as $key => $row) {
+// 			if (\strpos ($key, $qWord) !== false) {
+// 				$row['qWord'] = $qWord;
+// 				$row['key'] = $key;
+// 				$results[] = $row;
+// 			}
+// 		}
+// 	}
+// }
 
 // Get user weights
-$userWeights = array();
+$combinedResults = array();
 foreach ($results as $result) {
 	foreach ($result['users'] as $user) {
-		if (!isSet ($userWeights[$user])) {
-			$userWeights[$user] = $result['weight'];
+		if (!isSet ($combinedResults[$user])) {
+			$combinedResults[$user] = $result['weight'];
 		} else {
-			$userWeights[$user] += $result['weight'];
+			$combinedResults[$user] += $result['weight'];
 		}
 	}
 }
 
-\arsort ($userWeights, SORT_NUMERIC);
+\arsort ($combinedResults, SORT_NUMERIC);
 
 // Collect the relevant submissions and return
 $output = array();
-foreach ($userWeights as $username=>$weight) {
+foreach ($combinedResults as $username => $weight) {
 	$oUser = $oUserModel->get ($username);
 	$temp = $oSubmissionModel->get ($username, false);
 
