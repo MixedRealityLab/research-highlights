5b61472eb495918f77dddf97c36d31f06c1475ac
Martin Porcheron <martin@porcheron.io>
2015-01-30 14:30:07 +0000

    Missing file

---
+++ src_modes/submission/web/ajax/hitlist.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Research Highlights engine
+ * 
+ * Copyright (c) 2014 Martin Porcheron <martin@porcheron.uk>
+ * See LICENCE for legal information.
+ */
+
+// Fetch a list of users who have not submitted
+
+try {
+	print I::RH_User ()->getAll (null, function ($U) {
+		return !$U->latestVersion && $U->countSubmission;
+	})->toArrayJson ();
+} catch (\RH\Error $e) {
+	print $e->toJson ();
+}
