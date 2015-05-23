12b37dcfe09c0ba8e5a17e8ee69944bdc87e21d9
Martin Porcheron <martin@porcheron.uk>
2015-05-23 14:11:28 +0100

    Refer to index.php

---
+++ src/web/page/index.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Research Highlights engine
+ *
+ * Copyright (c) 2015 Martin Porcheron <martin@porcheron.uk>
+ * See LICENCE for legal information.
+ */
+
+require DIR_WPG . '/read.php';
